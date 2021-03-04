343fc72a6a4d6641e024ad39988f29e31e355098
임장순 <rosenari88@gmail.com>
2021-03-04 17:12:08 +0900

    Add / #8 / Material UI (Typescript) 적용

--- hanbit-front/pages/index.tsx
+++ hanbit-front/pages/index.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
-import Head from "next/head";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+		}
+	},
+}));
 
 export default function Home() {
-	const [text, setText] = useState<string>("자바스크립트");
-
-	setTimeout(() => {
-		setText("타입스크립트");
-	},1000);
+	const classes = useStyles();
 
 	return (
 		<div>
-			<div>
-				<span>{text} 적용완료</span>
+			<div className={classes.root}>
+				<Button variant="contained" color="secondary">버튼</Button>
 			</div>
 		</div>
 	);
