16ca331f83d07c71abc56f55d6e602d8d8d0034d
임장순 <rosenari88@gmail.com>
2021-03-05 18:08:59 +0900

    Add / #25 #54 / 찾아오시는길 페이지,네이버맵 컴포넌트 개발

--- hanbit-front/components/index.tsx
+++ hanbit-front/components/index.tsx
@@ -5,3 +5,4 @@ export { default as Cardslider } from './Slider/Cardslider';
 export { default as Cardmenu } from './Cardmenu';
 export { default as Shortcut } from './Shortcut';
 export { default as Label } from './Label';
+export { default as Navermap } from './Navermap';
