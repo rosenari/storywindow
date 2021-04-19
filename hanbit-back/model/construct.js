348d30d68bae84ba15f14041783ced09d7d6fd98
임장순 <rosenari88@gmail.com>
2021-04-19 23:22:41 +0900

    Add / getLikeConstructs - 좋아요 많은 시공모음 게시글 6개 출력 API

--- hanbit-back/model/construct.js
+++ hanbit-back/model/construct.js
@@ -1,15 +1,15 @@
 var db = require('../lib/db');
 
-module.exports = (function(){
-    
+module.exports = (function () {
+
     let construct = {};
-    
-    construct.getConstruct = (req,res,errhandler) => {
+
+    construct.getConstruct = (req, res, errhandler) => {
         let id = req.params.id;
-        
-        let sql = "select * from construct where id="+id;
-        
-        let handler = (req,res,rows) => {
+
+        let sql = "select * from construct where id=" + id;
+
+        let handler = (req, res, rows) => {
             let result = rows;
             let obj = new Object();
             obj.id = result[0].id;
@@ -18,14 +18,14 @@ module.exports = (function(){
             obj.tags = result[0].tags.split(",");
             obj.contents = result[0].contents;
             obj.space = result[0].space;
-            
-            res.json({result:obj});
+
+            res.json({ result: obj });
         }
-        
-        db.zero_get(sql,req,res,handler,errhandler);
+
+        db.zero_get(sql, req, res, handler, errhandler);
     }
-    
-    construct.getConstructs = (req,res,errhandler) => {
+
+    construct.getConstructs = (req, res, errhandler) => {
         ///:space/:tag/:fire/:auto/:order
         let startindex = req.params.index;
         let endindex = parseInt(startindex) + 16;
@@ -34,35 +34,35 @@ module.exports = (function(){
         let fire = req.params.fire;
         let auto = req.params.auto;
         let order = req.params.order;
-        
-        if(req.params.space=="all"){
+
+        if (req.params.space == "all") {
             space = "";
         }
-        
-        if(req.params.tag=="all"){
+
+        if (req.params.tag == "all") {
             tag = "";
         }
-        
-        if(fire=="true"){
+
+        if (fire == "true") {
             fire = encodeURIComponent("방염");
-        }else{
+        } else {
             fire = "";
         }
-        
-        if(auto=="true"){
+
+        if (auto == "true") {
             auto = encodeURIComponent("전동제품");
-        }else{
+        } else {
             auto = "";
         }
-        
-        let sql = "select c.id,c.title,c.contents,c.tags,c.space,c.views,c.date,c.mainimage,c.thumbnail,COUNT(cl.construct_id) as 'like' from construct as c left outer join construct_like as cl on c.id = cl.construct_id where c.tags like \"%"+tag+"%\" and c.tags like \"%"+fire+"%\" and c.tags like \"%"+auto+"%\" and c.space like \"%"+space+"%\" group by c.id order by '"+order+"' limit "+startindex+","+endindex;
-        
-        
-        let handler = (req,res,rows) => {
+
+        let sql = "select c.id,c.title,c.contents,c.tags,c.space,c.views,c.date,c.mainimage,c.thumbnail,COUNT(cl.construct_id) as 'like' from construct as c left outer join construct_like as cl on c.id = cl.construct_id where c.tags like \"%" + tag + "%\" and c.tags like \"%" + fire + "%\" and c.tags like \"%" + auto + "%\" and c.space like \"%" + space + "%\" group by c.id order by '" + order + "' limit " + startindex + "," + endindex;
+
+
+        let handler = (req, res, rows) => {
             let result = rows;
             let arr = new Array();
-            
-            for(let i in result){
+
+            for (let i in result) {
                 let obj = new Object();
                 obj.id = result[i].id;
                 obj.imgurl = result[i].thumbnail;
@@ -75,31 +75,62 @@ module.exports = (function(){
                 obj.date = date[0];
                 arr.push(obj);
             }
-            
+
             result = arr;
-            let sql2 = "select count(id) as allcount from construct where tags like \"%"+tag+"%\" and tags like \"%"+fire+"%\" and tags like \"%"+auto+"%\" and space like \"%"+space+"%\"";
-            
-            
-            let handler2 = (req,res,rows) => {
+            let sql2 = "select count(id) as allcount from construct where tags like \"%" + tag + "%\" and tags like \"%" + fire + "%\" and tags like \"%" + auto + "%\" and space like \"%" + space + "%\"";
+
+
+            let handler2 = (req, res, rows) => {
                 let allcount = rows[0].allcount;
-                let remain = allcount > parseInt(startindex)+result.length;  
-                res.json({"result":result,"allcount":allcount,"remain":remain});
+                let remain = allcount > parseInt(startindex) + result.length;
+                res.json({ "result": result, "allcount": allcount, "remain": remain });
             }
-            
-            db.zero_get(sql2,req,res,handler2,errhandler);
+
+            db.zero_get(sql2, req, res, handler2, errhandler);
         }
-        
-        db.zero_get(sql,req,res,handler,errhandler);
-    }
-    
-    function formatDate(date) { 
-        var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear(); 
-        if (month.length < 2) month = '0' + month; 
-        if (day.length < 2) day = '0' + day; return [year, month, day].join('-'); 
-        
+
+        db.zero_get(sql, req, res, handler, errhandler);
     }
 
-    
+    construct.getLikeConstructs = (req, res, errhandler) => {
+        let sql = `select c.id,c.title,c.contents,c.tags,c.space,c.views,c.date,c.mainimage,c.thumbnail,COUNT(cl.construct_id) as 'like'
+         from construct as c left outer join construct_like as cl
+         on c.id = cl.construct_id
+         group by c.id
+         order by COUNT(cl.construct_id) desc
+         limit 0,6;`
+
+        let handler = (req, res, rows) => {
+            let result = rows;
+            let arr = new Array();
+
+            for (let i in result) {
+                let obj = new Object();
+                obj.id = result[i].id;
+                obj.imgurl = result[i].thumbnail;
+                obj.title = result[i].title;
+                obj.tags = result[i].tags.split(",");
+                obj.space = result[i].space.split(",");
+                obj.like = result[i].like;
+                obj.views = result[i].views;
+                let date = formatDate(result[i].date).toString().split("T");
+                obj.date = date[0];
+                arr.push(obj);
+            }
+
+            res.json({ result: arr });
+        }
+
+        db.zero_get(sql, req, res, handler, errhandler);
+    }
+
+    function formatDate(date) {
+        var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day; return [year, month, day].join('-');
+    }
+
+
     return construct;
-    
+
 })();
