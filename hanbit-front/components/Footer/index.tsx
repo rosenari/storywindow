b8650374b89883c22bd52822acee09b9a2cfcefe
임장순 <rosenari88@gmail.com>
2021-06-27 18:53:13 +0900

    Add / 사업자번호 등록

--- hanbit-front/components/Footer/index.tsx
+++ hanbit-front/components/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
                 <div className={styles.footer_inner_container_left}>
                     <div className={styles.footer_menu_1}>회사소개&nbsp;&nbsp;{/*<strong>개인정보처리방침</strong>&nbsp;&nbsp;이용약관&nbsp;&nbsp;*/}사업자정보</div>
                     <div className={styles.footer_menu_2}><span className="call_text"><IoIosCall />042-581-2464</span>&nbsp;&nbsp; 월-토 09:00 - 21:00 | 일요일 휴무 | 팩스 042-545-7220 | 이메일 limwj2464@naver.com </div>
-                    <div className={styles.footer_menu_3}>대표: 임원재 | 주소: 대전광역시 유성구 원내동 296-9(홈데코갤러리 1층) | 사업자등록번호: 000-00-00000</div>
+                    <div className={styles.footer_menu_3}>대표: 임원재 | 주소: 대전광역시 유성구 원내동 296-9(홈데코갤러리 1층) | 사업자등록번호: 768-15-01230</div>
                     <div className={styles.footer_menu_4}>스토리 창은 기쁨과 감동을 드리는 커튼블라인드 기업입니다.</div>
                 </div>
                 <div className={styles.footer_inner_container_right}>
