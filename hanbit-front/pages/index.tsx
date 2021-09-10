2332a85d957ef75b0cb21802a8f5700a571a3b36
임장순 <rosenari88@gmail.com>
2021-09-10 21:15:50 +0900

    [STRY-19] 파트너 주요 업종메뉴 추가

--- hanbit-front/pages/index.tsx
+++ hanbit-front/pages/index.tsx
@@ -5,7 +5,23 @@ import { NextPage } from "next";
 import { Context } from "vm";
 import axios from "axios";
 import FadeIn from "react-fade-in";
-import styles from "./css/index.module.css";
+import styles from "./css/index.module.scss";
+
+const IMG_URL = {
+	BIG_LOGO: '/images/storylogo_big.png',
+	INTERIOR_VECTOR: '/images/interior_vector.png',
+	BLIND_VECTOR: '/images/blind_vector.png'
+}
+const TAGS = ['#전국블라인드도매', '#공장제작', '#전국최저가', '#소량납품환영'].map((tag, i) => ({ id: i + 1, tag}));
+const INTERIOR_DESCRIPTION = [
+	'ㆍ인테리어 완성을 위한 고품질의 블라인드를 전국 최저가에 제공해드립니다.',
+	'ㆍ공장에서 정확한 치수로 직접 제작하여, 인테리어의 완성도를 높여드립니다.'
+].map((desc, i) => ({ id: i + 1, desc}));
+const BLIND_DESCRIPTION = [
+	'ㆍ소규모 블라인드 파트너를 적극 지원합니다.',
+	'ㆍ단 한개의 납품도 최선을 다해 진행합니다.',
+	'ㆍ함께하는 성장을 추구합니다.'
+].map((desc, i) => ({ id: i + 1, desc}));
 
 interface HomeProps {
 	rankData: any
@@ -21,10 +37,7 @@ const Home: NextPage<HomeProps> = (props) => {
 					스토리창 <span className={styles.main}>전국 파트너</span> 모집
 				</div>
 				<div className={styles.tag_main}>
-					<span className={styles.tag}>#전국블라인드도매</span>
-					<span className={styles.tag}>#공장제작</span>
-					<span className={styles.tag}>#전국최저가</span>
-					<span className={styles.tag}>#소량납품환영</span>
+					{TAGS.map(v => <span key={v.id} className={styles.tag}>{v.tag}</span>)}
 				</div>
 				<div className={styles.recruit_description} style={{ marginTop:'100px' }}>
 					ㆍ <span className={styles.main}>공장</span>에서 블라인드를 직접 제작하여, <span className={styles.main}>전국 최저가</span>로 납품해드립니다
@@ -37,19 +50,54 @@ const Home: NextPage<HomeProps> = (props) => {
 				</div>
 				</FadeIn>
 				<div className={styles.logo_box}>
-						<Image url={'/images/storylogo_big.png'} width={300} height={300} />
+						<Image url={IMG_URL.BIG_LOGO} width={300} height={300} />
 				</div>
 			</div>
 			<div className={styles.card_main}>
 				<div className={styles.card_slider_header}>
-				<strong> 스토리 창
-					<span style={{ color: "var(--color-main)" }}> 파트너 시공</span>
-				</strong>
+					<strong> 스토리 창
+						<span style={{ color: "var(--color-main)" }}> 파트너 시공</span>
+					</strong>
 				<span className={styles.card_slider_header_more} onClick={() => {
 						Router.push('/construct/list');
 					}}>더보기</span>
 				</div>
 				<Cardslider data={props.rankData.result} />
+			</div>
+			<div className={styles.industry_main}>
+				<div className={styles.industry_header}>
+					<strong> 
+						<span style={{ color: "var(--color-main)" }}>파트너</span> 주요 업종
+					</strong>
+				</div>
+				<div className={styles.industry_body}>
+					<div className={[styles.box, styles.interior].join(' ')}>
+						<div className={styles.header}>
+							<span className={styles.text}>인테리어</span>
+						</div>
+						<div className={styles.body}>
+							<div className={styles.description_box}>
+								{INTERIOR_DESCRIPTION.map(v => <div key={v.id} className={styles.description}>{v.desc}</div>)}
+							</div>
+							<div className={styles.img_box}>
+								<img src={IMG_URL.INTERIOR_VECTOR} width={200} />
+							</div>
+						</div>
+					</div>
+					<div className={[styles.box, styles.blind].join(' ')}>
+						<div className={styles.header}>
+							<span className={styles.text}>블라인드</span>
+						</div>
+						<div className={styles.body}>
+							<div className={styles.description_box}>
+								{BLIND_DESCRIPTION.map(v => <div key={v.id} className={styles.description}>{v.desc}</div>)}
+							</div>
+							<div className={styles.img_box}>
+								<img src={IMG_URL.BLIND_VECTOR} width={200} />
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
