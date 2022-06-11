96642b91083c6eddaadd858836a0eb00355a0fae
rosenari <rosenari88@gmail.com>
2022-06-11 22:13:16 +0900

    메뉴 모바일 개발

--- story-front/components/Header/Mobile/Menu/index.tsx
+++ story-front/components/Header/Mobile/Menu/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './index.module.css';
+import Router from "next/router";
+import styles from './index.module.scss';
 import { classFor } from '@/util/index';
 
 
@@ -12,11 +13,20 @@ const get_open_state_style = (opened: boolean) => {
     return opened? styles.opened : styles.closed;
 }
 
+const menus = [
+    { title: '홈', url: '/' },
+    { title: '회사소개', url: '/intro' }, 
+    { title: '납품업체 시공모음', url: '/product/list' }
+].map((v, i) => { return { ...v, no: i } });
+
 const Menu: React.FC<MenuProps> = ({ menu_opened, setOpen }) => {
 
     return (
         <div className={classFor([styles.menu_container, get_open_state_style(menu_opened)])} onClick={() => setOpen(false)}>
-            <div className={classFor([styles.menu, get_open_state_style(menu_opened)])}>{"Menu"}</div>
+            <div className={classFor([styles.menu, get_open_state_style(menu_opened)])}>
+                {menus.map(menu => { return <div key={menu.no} onClick={() => { Router.push(menu.url) }} className={styles.menu_button}>{menu.title}</div> })}
+            </div>
+            {menu_opened && <div className={styles.close_hint}>{"해당 영역을 클릭하면 메뉴가 닫힙니다."}</div>}
         </div>
     )
 }
